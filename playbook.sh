9284d4a40934cffc76d3715d20b78d1f68de0cb3
Jason Worthen <jason@worthen.org>
2022-05-01 14:24:57 -0500

    update run script

--- playbook.sh
+++
@@ -1,1 +0,0 @@
-ansible-playbook -i ./inventory/hosts.yml -b -K ./playbooks/$1.yml --limit $2
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [ $# != 0 ]
+  then 
+    echo 'Please pass a variable of the playbook you want to run. You can also, optionally, specifyc a specific server to limit the scope.'
+elif [ $# = 1 ]
+  ansible-playbook -i ./inventory/hosts.yml -b -K ./playbooks/$1.yml
+else
+  ansible-playbook -i ./inventory/hosts.yml -b -K ./playbooks/$1.yml --limit $2
+fi
